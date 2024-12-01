bc47745e6ad898576c01fe107398f90b8ba8d29d
Nils Hartmann <nils@nilshartmann.net>
2024-12-01 18:10:16 +0100

    react19 und demo getrennt

---
+++ react19-demo-app/frontend/vite.config.ts
@@ -0,0 +1,52 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const enableCompiler = false;
+
+const apiBackend = process.env.API_BACKEND ?? "http://localhost:3002";
+console.log("API Backend", apiBackend);
+
+const babelConfig = enableCompiler
+  ? { babel: { plugins: ["babel-plugin-react-compiler", {}] } }
+  : undefined;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(babelConfig), TanStackRouterVite()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: apiBackend,
+        changeOrigin: true,
+        xfwd: true,
+        // configure: (proxy, _options) => {
+        //   proxy.on("error", (err, _req, _res) => {
+        //     console.log("proxy error", err);
+        //   });
+        //   proxy.on("proxyReq", (proxyReq, req, _res) => {
+        //     console.log(
+        //       "Sending Request:",
+        //       req.method,
+        //       req.url,
+        //       " => TO THE TARGET =>  ",
+        //       proxyReq.method,
+        //       proxyReq.protocol,
+        //       proxyReq.host,
+        //       proxyReq.path,
+        //     );
+        //   });
+        //   proxy.on("proxyRes", (proxyRes, req, _res) => {
+        //     console.log(
+        //       "Received Response from the Target:",
+        //       proxyRes.statusCode,
+        //       req.url,
+        //     );
+        //   });
+        // },
+      },
+    },
+  },
+});
